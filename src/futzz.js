e90f53f312a863197c144d69cf3a4761f8a821a5
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2020-11-25 08:03:11 +0000

    "Improving code usability"

--- src/futzz.js
+++ src/futzz.js
@@ -1,154 +1,234 @@
-export function lz(docStr = '', dict = new Map(), name = 'unknown doc') {
-  const factors = [];
-  let codeId = dict.size/2;
-  let wordFirstIndex = -1;
-  let charIndex = 0;
-  let currentWord = '';
+const MAX_ITERATION = 12;
+const MAX_ENT = 0;
+const MAX_TOT_ENT = 1;
+const TERMINATE_ON = MAX_ENT;
 
-  // a tiny bit of preProcessing
+const State = {
+  dict: new Map(),
+  indexHistory: []
+};
 
-  docStr = docStr.replace(/\p{P}+/gu, '');     // unicode replace all punctuation
-  docStr = docStr.replace(/\p{Z}+/gu, ' ');     // unicode replace all separators
-  docStr = docStr.trim().toLocaleLowerCase();
+  export function index(text, name, useRun = false) {
+    const Ent = [];
+    const sortKey = useRun ? 'count' : 'runCount';
 
-  factors.docStr = docStr;
+    const indexHistoryEntry = {
+      docName: name, 
+      terminatorCondition: TERMINATE_ON == MAX_ENT ? 'maxEntropy' : 
+        TERMINATE_ON == MAX_TOT_ENT ? 'maxTotalEntropy' : 
+        'unknown',
+      useRunCount: useRun,
+      indexStart: new Date
+    };
 
-  // this is how simple lz is, isn't it beautiful? :)
+    let dict, factors, maxEntropy = 0, maxFactors;
 
-    for ( const nextChar of docStr ) {
-      if ( ! dict.has(nextChar) ) {
-          const data = {
-            name,
-            word: nextChar,
-            firstIndex: charIndex,
-            count: 0,
-            codeId 
+    indexingCycle: for( let i = 0; i < MAX_ITERATION; i++ ) {
+      ({dict, factors} = lz(text, State.dict, name)); 
+      const entropy = ent(factors, useRun ? i+1 : undefined);
+      const total = entropy*factors.length;
+      Ent.push({entropy, total: entropy*factors.length, name});
+      switch( TERMINATE_ON ) {
+        case MAX_ENT: {
+          if ( entropy > maxEntropy ) {
+            maxFactors = factors;
+            maxEntropy = entropy;
+          } else {
+            break indexingCycle;
           }
-          dict.set(codeId, data);
-          dict.set(nextChar, data);
-          codeId += 1;
-      }
-      if ( ! dict.has(currentWord) ) {
-        // save the new unseen token
-          const data = {
-            name, 
-            word: currentWord,
-            firstIndex: null,
-            count: 0,
-            codeId 
+        } break;
+        case MAX_TOT_ENT: {
+          if ( total > maxEntropy ) {
+            maxEntropy = total;
+            maxFactors = factors;
+          } else {
+            break indexingCycle;
           }
-          dict.set(codeId, data);
-          dict.set(currentWord, data);
-          codeId += 1;
-
-        // get the factor 
-          let suffix = '';
-          if ( currentWord.length ) {
-            const lastWord = currentWord.slice(0,-1);
-            suffix = currentWord.slice(-1);
-            const factor = dict.get(lastWord);
-
-            if ( factor.count == 0 ) {
-              factor.firstIndex = wordFirstIndex;
-            }
-            factor.count++;
-
-            factors.push(factor);
-          }
-
-        // update the state
-          wordFirstIndex = charIndex;
-          currentWord = suffix;
-      }
-
-      currentWord += nextChar;
-      charIndex += 1;
-    }
-
-    // empty any state into the dictionary and factors list
-      if ( ! dict.has(currentWord) ) {
-        // save the new unseen token
-          const data = {
-            word: currentWord,
-            firstIndex: null,
-            count: 0,
-            codeId 
-          }
-          dict.set(codeId, data);
-          dict.set(currentWord, data);
-          codeId += 1;
-
-        // get the factor 
-          let suffix = '';
-          if ( currentWord.length ) {
-            const lastWord = currentWord.slice(0,-1);
-            suffix = currentWord.slice(-1);
-            const factor = dict.get(lastWord);
-
-            if ( factor.count == 0 ) {
-              factor.firstIndex = wordFirstIndex;
-            }
-            factor.count++;
-
-            factors.push(factor);
-
-            // in this case we push the last factor if any
-              const suffixFactor = dict.get(suffix);
-              factors.push(suffixFactor);
-          }
-      } else {
-        const factor = dict.get(currentWord);
-        if ( factor.count == 0 ) {
-          factor.firstIndex = wordFirstIndex;
         }
-        factor.count++;
+      } 
+    }
 
-        factors.push(factor);
+    indexHistoryEntry.indexEndAt = new Date;
+
+    State.indexHistory.push(indexHistoryEntry);
+
+    console.log(name, Ent.map(({entropy, total}) => `${entropy.toFixed(2)} : ${total.toFixed(2)}`));
+
+    return {dict, factors: maxFactors || factors};
+  }
+
+  export function query(words) {
+    const {dict} = State;
+    const {factors} = lz(words, dict, 'query');
+
+    factors.forEach(({name, word}) => {
+      console.log(JSON.stringify({word, name:Object.fromEntries([...name.entries()].map(([_,{count}]) => [_, count]))}));
+    });
+    console.log('');
+  }
+
+  export function lz(docStr = '', dict = new Map(), name = 'unknown doc') {
+    const factors = [];
+    let codeId = dict.size/2;
+    let wordFirstIndex = -1;
+    let charIndex = 0;
+    let currentWord = '';
+
+    // a tiny bit of preProcessing
+
+    docStr = docStr.replace(/\p{P}+/gu, '');     // unicode replace all punctuation
+    docStr = docStr.replace(/\p{Z}+/gu, ' ');     // unicode replace all separators
+    docStr = docStr.trim().toLocaleLowerCase();
+
+    factors.docStr = docStr;
+
+    // this is how simple lz is, isn't it beautiful? :)
+
+      for ( const nextChar of docStr ) {
+        if ( ! dict.has(nextChar) ) {
+            const data = {
+              name: new Map([[name, {count:0}]]),
+              word: nextChar,
+              firstIndex: charIndex,
+              count: 0,
+              codeId 
+            }
+            dict.set(codeId, data);
+            dict.set(nextChar, data);
+            codeId += 1;
+        }
+        if ( ! dict.has(currentWord) ) {
+          // save the new unseen token
+            const data = {
+              name: new Map([[name, {count:0}]]),
+              word: currentWord,
+              firstIndex: null,
+              count: 0,
+              codeId 
+            }
+            dict.set(codeId, data);
+            dict.set(currentWord, data);
+            codeId += 1;
+
+          // get the factor 
+            let suffix = '';
+            if ( currentWord.length ) {
+              const lastWord = currentWord.slice(0,-1);
+              suffix = currentWord.slice(-1);
+              const factor = dict.get(lastWord);
+
+              if ( factor.count == 0 ) {
+                factor.firstIndex = wordFirstIndex;
+              }
+              if ( !factor.name.has(name) ) {
+                factor.name.set(name, {count: 1});
+              } else {
+                factor.name.get(name).count += 1;
+              }
+              factor.count++;
+
+              factors.push(factor);
+            }
+
+          // update the state
+            wordFirstIndex = charIndex;
+            currentWord = suffix;
+        }
+
+        currentWord += nextChar;
+        charIndex += 1;
       }
 
-  return {factors, dict};
-}
+      // empty any state into the dictionary and factors list
+        if ( ! dict.has(currentWord) ) {
+          // save the new unseen token
+            const data = {
+              name: new Map([[name, {count:0}]]),
+              word: currentWord,
+              firstIndex: null,
+              count: 0,
+              codeId 
+            }
+            dict.set(codeId, data);
+            dict.set(currentWord, data);
+            codeId += 1;
 
-export function ent(factors, run = 1, adjustLength = false) {
-  let TotalLength = 0;
-  let Ent = 0;
+          // get the factor 
+            let suffix = '';
+            if ( currentWord.length ) {
+              const lastWord = currentWord.slice(0,-1);
+              suffix = currentWord.slice(-1);
+              const factor = dict.get(lastWord);
 
-  run = run || 1;
-  
-  const dict = new Map(); 
+              if ( factor.count == 0 ) {
+                factor.firstIndex = wordFirstIndex;
+              }
+              factor.count++;
 
-  for( const f of factors ) {
-    if ( !dict.has(f.word) ) {
-      dict.set(f.word, f);
-      f.runCount = 0;
+              factors.push(factor);
+
+              // in this case we push the last factor if any
+                const suffixFactor = dict.get(suffix);
+                factors.push(suffixFactor);
+            }
+        } else {
+          const factor = dict.get(currentWord);
+          if ( factor.count == 0 ) {
+            factor.firstIndex = wordFirstIndex;
+          }
+          if ( !factor.name.has(name) ) {
+            factor.name.set(name, {count: 1});
+          } else {
+            factor.name.get(name).count += 1;
+          }
+          factor.count++;
+
+          factors.push(factor);
+        }
+
+    return {factors, dict};
+  }
+
+  export function ent(factors, run = 1, adjustLength = false) {
+    let TotalLength = 0;
+    let Ent = 0;
+
+    run = run || 1;
+    
+    const dict = new Map(); 
+
+    for( const f of factors ) {
+      if ( !dict.has(f.word) ) {
+        dict.set(f.word, f);
+        f.runCount = 0;
+      }
+      f.runCount += 1;
+      TotalLength += f.word.length;
     }
-    f.runCount += 1;
-    TotalLength += f.word.length;
-  }
 
-  if ( adjustLength ) {
-    TotalLength *= run;
-  }
-
-  for( const {runCount,count,word} of dict.values() ) {
-    let Count = runCount;
-    if ( run > 1 ) {
-      Count = count; 
+    if ( adjustLength ) {
+      TotalLength *= run;
     }
-    const p = Count*word.length/TotalLength;
-    const ent = -p*Math.log2(p);
-    Ent += ent;
+
+    for( const {runCount,count,word} of dict.values() ) {
+      let Count = runCount;
+      if ( run > 1 ) {
+        Count = count; 
+      }
+      const p = Count*word.length/TotalLength;
+      const ent = -p*Math.log2(p);
+      Ent += ent;
+    }
+
+    let check;
+
+    if ( adjustLength ) {
+      check = factors.docStr.length*run == TotalLength;
+    } else {
+      check = factors.docStr.length == TotalLength;
+    }
+
+    console.assert(check, factors.docStr.length*run, TotalLength);
+
+    return Ent;
   }
-
-  let check;
-
-  if ( adjustLength ) {
-    check = factors.docStr.length*run == TotalLength;
-  } else {
-    check = factors.docStr.length == TotalLength;
-  }
-
-  console.assert(check, factors.docStr.length*run, TotalLength);
-
-  return Ent;
-}
