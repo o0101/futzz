968a7dae1d8f7a5163de0da5a2b006bb1d5e35f2
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2020-12-06 07:18:52 +0000

    "OK"

--- src/futzz.js
+++ src/futzz.js
@@ -414,9 +414,9 @@ export const State = {
   }
 
   function simplify(str) {
+    str = str.replace(/[\n\r\t\0\b]+/gu, ' ');     // unicode replace all ASCII noise -> single space
     str = str.replace(/\p{P}+/gu, ' ');            // unicode replace all punctuation -> single space
     str = str.replace(/\p{Z}+/gu, ' ');            // unicode replace all separators -> single space
-    str = str.replace(/[\n\r\t\0\b]+/gu, ' ');     // unicode replace all ASCII noise -> single space
     str = str.trim();
     str = str.toLocaleLowerCase();
 
