28df6dbc891f3b9336e3b11c0adaa4aefd79c5f0
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2020-12-13 12:39:28 +0000

    "OK"

--- tests/run.js
+++ tests/run.js
@@ -117,7 +117,7 @@ async function start() {
   async function runMultiAuto(limit = 'nolimit') {
     const allConfigs = enumerateConfigs(PARAM_RANGES);
     //console.log(JSON.stringify({allConfigs}));
-    const POOL_SIZE = Math.max(7,os.cpus().length - 4); // 1 for OS and 1 for this process, and 2 spare
+    const POOL_SIZE = Math.min(7,os.cpus().length - 4); // 1 for OS and 1 for this process, and 2 spare
     const runner = [];
     let notifyComplete;
     let running = 0;
